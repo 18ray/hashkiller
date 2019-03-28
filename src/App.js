dc4f8979cefd7882f2b167412c544919cbd6da09
18ray <ray.yustian@gmail.com>
2019-03-28 13:58:10 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import axios from 'axios';
+
+import EncryptBar from './components/EncryptBar';
+import DecryptBar from './components/DecryptBar';
 
 class App extends Component {
+  state = { enrypt: '' }
+
+  onEncryptSubmit = async (term) => {
+    const response = await axios.get(`https://indosec.web.id/api/ultra_hash.php?action=custom&value=${term}&type=md5`)
+
+    this.setState({ encrypt: response.data.data.md5 });
+  }
+
+  onDecryptSubmit = async (term) => {
+    const response = await axios.get(`https://indosec.web.id/api/ultra_hash.php?action=dec&value=${term}`)
+
+    console.log(response)
+    this.setState({ decrypt: response.data.data });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="ui container">
+        <EncryptBar onSubmit={this.onEncryptSubmit} />
+        MD5: {this.state.encrypt}
+        <DecryptBar onSubmit={this.onDecryptSubmit} />
+        Plaintext: {this.state.decrypt}
       </div>
     );
   }
